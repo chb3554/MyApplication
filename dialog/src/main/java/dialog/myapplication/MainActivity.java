396ed5299dcc747c8b719b9938812032fd224b38
kimbumyoung <qjadud22@naver.com>
2015-11-05 12:05:02 +0900

    Initial commit

---
+++ dialog/src/main/java/dialog/myapplication/MainActivity.java
@@ -0,0 +1,84 @@
+package dialog.myapplication;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    TextView tvName, tvEmail;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("사용자 정보 입력");
+
+        tvName = (EditText) findViewById(R.id.tvName);
+        tvEmail = (EditText) findViewById(R.id.tvEmail);
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialogView = (View) View.inflate(MainActivity.this,
+                        R.layout.dialog1, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(
+                        MainActivity.this);
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.drawable.img2);
+                dlg.setView(dialogView);
+
+                dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+                dlgEdtName.setText(tvName.getText().toString());
+                dlgEdtEmail.setText(tvEmail.getText().toString());
+
+                dlg.setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                                dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+                                tvName.setText(dlgEdtName.getText().toString());
+                                tvEmail.setText(dlgEdtEmail.getText().toString());
+
+                            }
+                        });
+
+                dlg.setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                Toast toast = new Toast(MainActivity.this);
+                                Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+                                int xOffset = (int) (Math.random() * display.getWidth());
+                                int yOffset = (int) (Math.random() * display.getHeight());
+
+                                toast.setGravity(Gravity.TOP | Gravity.LEFT,xOffset, yOffset);
+                                toastView = (View) View.inflate(
+                                        MainActivity.this,
+                                        R.layout.toast1, null);
+                                toastText = (TextView) toastView
+                                        .findViewById(R.id.toastText1);
+                                toastText.setText("취소했습니다");
+                                toast.setView(toastView);
+                                toast.show();
+                            }
+                        });
+                dlg.show();
+            }
+        });
+    }
+}
